23b693536c3073897e5632d6321de113a291b48c
sebastianponce-sucurinet <sebastian.ponce@sucuri.net>
2025-05-28 13:20:58 -0300

    Updates for build

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,34 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import path from 'path'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  build: {
+    target: 'esnext',
+    minify: 'esbuild',
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'vendor': ['react', 'react-dom', 'react-router-dom'],
+          'axios': ['axios'],
+          'apollo': ['@apollo/client'],
+          'i18n': ['i18next', 'react-i18next'],
+        }
+      }
+    },
+    chunkSizeWarningLimit: 1000,
+  },
+  optimizeDeps: {
+    include: ['axios', '@apollo/client', 'i18next', 'react-i18next'],
+    exclude: ['@apollo/client/core']
+  },
   server: {
     proxy: {
       '/api/cj': {
