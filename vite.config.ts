612ea3d07908e02ef9046dfff07a2ac19b421ae5
sebastianponce-sucurinet <sebastian.ponce@sucuri.net>
2025-05-28 15:48:26 -0300

    Updates for build

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
-  base: '/gd-front/dist/',
+  base: '/',
   build: {
     target: 'es2015',
     minify: 'esbuild',
